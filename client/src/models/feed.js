563801570d82152c541b0c279a37aac0f97ee984
Christopher Brady <bradyc4@hawkmail.newpaltz.edu>
2020-10-09 16:34:52 -0400

    added posts and data

---
+++ client/src/models/feed.js
@@ -0,0 +1,24 @@
+/*
+    Feed Model. The raw data for the feed display.
+    Currently mocked at the client side.
+*/
+export const posts = [
+    {
+        url: 'https://openthread.google.cn/images/ot-contrib-google.png',
+        message: '',
+        owner: {
+            name: 'John Smith',
+            handle: 'johnsmith',
+            profile: 'https://wi-images.condecdn.net/image/jgov7eBrRvb/crop/2040/f/6-facial-recognition-hero.jpg'
+        }
+    },
+    {
+        url: 'https://www.wfla.com/wp-content/uploads/sites/71/2019/07/Babyshoujo-1145429588220108800-20190630_163109-img1.jpg?w=1280',
+        message: '',
+        owner: {
+            name: 'John Smith',
+            handle: 'johnsmith',
+            profile: 'https://wi-images.condecdn.net/image/jgov7eBrRvb/crop/2040/f/6-facial-recognition-hero.jpg'
+        }
+    },
+];
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -3,7 +3,9 @@
         <h2 class="title is-2">Feed Page</h2>
         <div class="columns">
             <div class="column is-two-thirds">
-                <Post /><Post /><Post /><Post />
+                <Post v-for=" (x, i) in posts " 
+                       :key="i"
+                       :post="x" />
             </div>
             <div class="column is-one-third">
                 <Sidebar />
@@ -15,7 +17,14 @@
 <script>
 import Sidebar from "@/components/Sidebar";
 import Post from "@/components/Post";
+import { posts } from "@/models/feed";
+
 export default {
+    data(){
+        return {
+            posts
+        }
+    },
     components: {
         Sidebar, Post
     }
