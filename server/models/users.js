9df2b7c1a94dd31ea8bf4a6919e4a5f31d6a77a5
Christopher Brady <bradyc4@hawkmail.newpaltz.edu>
2020-10-20 23:00:59 -0400

    server side

---
+++ server/models/users.js
@@ -0,0 +1,10 @@
+const data = [{name: 'Christopher', age: 25},{name: 'Biden', age: 78}]
+function getAll(){
+    return data;
+}
+
+function add(name, age){
+    data.push({name, age});
+}
+
+module.exports = { getAll, add, search: q => data.filter(x=> x.name == q) }
